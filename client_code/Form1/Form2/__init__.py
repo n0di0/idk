b4dc35d4c4ad35720147b3e7d26744496c72fdf8
nrapheal04@gmail.com <nrapheal04@gmail.com>
2026-03-20 18:28:04 +0000

    fixed embed resolution

--- client_code/Form1/Form2/__init__.py
+++ client_code/Form1/Form2/__init__.py
@@ -9,6 +9,6 @@ class Form2(Form2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    iframe = jQuery("<iframe width='392px' margin-top: 100px; height='450px'>").attr("src","https://studio.code.org/projects/applab/NONz21j9-O9OI-2j9E-zT9F-b1j69EFO6vzz/embed?nosource")
+    iframe = jQuery("<iframe width='392px' height='620px'>").attr("src","https://studio.code.org/projects/applab/NONz21j9-O9OI-2j9E-zT9F-b1j69EFO6vzz/embed?nosource")
     iframe.appendTo(get_dom_node(self.content_panel))
     # Any code you write here will run when the form opens.
